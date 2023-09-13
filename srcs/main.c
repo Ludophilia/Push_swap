754c7f21d44cea3a89f6e8e86108665ab0c3f861
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-09-13 19:43:45 +0200

    Done implementing pre sorting, but it's not norm compliant

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 14:16:34 by jgermany          #+#    #+#             */
-/*   Updated: 2023/09/13 15:41:03 by jgermany         ###   ########.fr       */
+/*   Updated: 2023/09/13 19:43:08 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,57 +40,39 @@ static void	tmp_traverse_instructions(t_list *start)
 	}
 }
 
-int	sort_upto_100nbs(t_stk *stackA, t_stk *stackB, t_list **instrs)
+// int	sort_presort_100(t_stk *stackA, t_stk *stackB, t_list **instrs)
+// {
+// }
+
+int	sort_upto_100nbs(t_stk **stacks, t_list **instrs)
 {
-	// Pre sorting:
-	// Everything on stack a to stack b.
-	//		
-	// [30 * 0 ; 30 * 1[ (0 ; 29)	
-	// [30 * 1 ; 30 * 2[ (30 ; 59)
-	// [30 * 2 ; 30 * 3[ (60 ; 89)
-	// [30 * 3 ; 30 * 4[ (90 ; 119)
-	// ...
-	// [PIVOT * i ; PIVOT * i + 1[ (i == 0)
-	// ...
-	
-	// except if the nbs are: (init_size - 1, init_size - 2, init_size - 3)
-	// sort those numbers.
-	int	max[3];
 	int	ij[2];
-	int	pivot;
-	int	node;
-	
-	max[0] = (stackA->size - 1);
-	max[1] = (stackA->size - 2);
-	max[2] = (stackA->size - 3);
-	pivot = 30;
+	int	pivot[3];
+	int	node[2];
+
+	node[1] = stacks[0]->size - 3;
+	pivot[2] = stacks[0]->size / 2;
 	ij[0] = 0;
-	// ft_printf("stackA->size == %i\n", stackA->size);
-	while (stackA->size > 3)
+	while (stacks[0]->size > 3)
 	{
-		// ft_printf("New turn.\n");
-		// ft_printf("pivot == %i ; pivot + 1 == %i\n", pivot * ij[0],
-			// pivot * (ij[0] + 1));
-		ij[1] = -1;;
-		while (++ij[1] < stackA->size && stackA->size > 3)
+		ij[1] = stacks[0]->size;
+		while (ij[1]-- > 0)
 		{
-			// ft_printf("current == %i\n", *(int *)(*stackA->head)->content);
-			node = *(int *)(*stackA->head)->content;
-			if (node == max[0] || node == max[1] || node == max[2])
+			node[0] = *(int *)(*stacks[0]->head)->content;
+			pivot[0] = (pivot[2] * ij[0]);
+			pivot[1] = pivot[2] * (ij[0] + 1);
+			if (node[0] < node[1] && node[0] >= pivot[0] && node[0] < pivot[1])
 			{
-				if (game_rotate(stackA, 0, instrs) == -1)
+				if (game_push(stacks[0], stacks[1], instrs) == -1)
+					return (-1);
+				if ((node[0] >= (pivot[0] + (pivot[1] - pivot[0]) / 2)
+						&& game_rotate(stacks[1], 0, instrs) == -1))
 					return (-1);
 			}
-			else if (node >= (pivot * ij[0]) && node < (pivot * (ij[0] + 1)))
-			{
-				if (game_push(stackA, stackB, instrs) == -1)
+			else
+				if (game_rotate(stacks[0], 0, instrs) == -1)
 					return (-1);
-				if ((node >= (pivot * ij[0]) / 2)
-					&& game_rotate(stackB, 0, instrs) == -1)
-					return (-1);
-			}
 		}
-		// tmp_traverse_stack(stackA);
 		ij[0]++;
 	}
 	return (0);
@@ -113,7 +95,7 @@ int	main(int argc, char **argv)
 	// tmp_traverse_stack(stacks[0]);
 	// tmp_traverse_stack(stacks[1]);
 	(void)tmp_traverse_stack;
-	sort_upto_100nbs(stacks[0], stacks[1], instrs);
+	sort_upto_100nbs(stacks, instrs);
 	// sort_upto_5nbs(stacks[0], stacks[1], instrs);
 	tmp_traverse_instructions(*instrs);
 	// tmp_traverse_stack(stacks[0]);
